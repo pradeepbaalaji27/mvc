0a81d722246a6a079b96bc4477290baea271fd25
Pradeep Baalaji <Pradeep Baalaji@DESKTOP-A0O3SJC>
2019-03-06 13:05:27 +0530

    Project mvc

---
+++ Demo/src/controller/LoginController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username=request.getParameter("username");
+		String password=request.getParameter("passwd");
+		
+		//Initilizing the LoginCredential object
+		model.LoginCredential loginObj=new model.LoginCredential();
+		loginObj.setUsername(username);
+		loginObj.setPassword(password);
+		
+		//Checking the Login & Password
+		if(loginObj.checkLogin())
+		{
+			request.setAttribute("username",username);
+			RequestDispatcher dispatch=request.getRequestDispatcher("Success.jsp");
+			dispatch.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher dispatch=request.getRequestDispatcher("Failure.jsp");
+			dispatch.forward(request, response);
+		}	
+	}
+
+}
